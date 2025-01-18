33843845b3c962ccb06ff8cdfb06e1ac5f686975
aemorozov <admin@aemorozov.com>
2025-01-18 11:22:57 +0200

    some fix to menu animation

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -5,35 +5,48 @@ import Image from 'next/image';
 
 export const Menu = () => {
   return (
-    <div className={classes.mainBlock}>
-      <Link
-        href={'news'}
-        className={classes.menuItem + ' ' + classes.hoverItem}
-      >
-        Разряды{' '}
-        <Image
-          src={arrowDown}
-          className={classes.arrowDown}
-          alt="Arrow down"
-          width={16}
-          height={15}
-        />
-      </Link>
-      <Link href={'news'} className={classes.menuItem}>
-        VIP-Кабинеты
-      </Link>
-      <Link href={'news'} className={classes.menuItem}>
-        Услуги
-      </Link>
-      <Link href={'news'} className={classes.menuItem}>
-        Новости и акции
-      </Link>
-      <Link href={'news'} className={classes.menuItem}>
-        Кухня и бар
-      </Link>
-      <Link href={'services'} className={classes.menuItem}>
-        Контакты
-      </Link>
-    </div>
+    <>
+      <div className={classes.mainBlock}>
+        <Link
+          href={'news'}
+          className={classes.menuItem + ' ' + classes.hoverItem}
+        >
+          Разряды{' '}
+          <Image
+            src={arrowDown}
+            className={classes.arrowDown}
+            alt="Arrow down"
+            width={16}
+            height={15}
+          />
+        </Link>
+        <div className={classes.mainBlockSecondMenu}>
+          <Link href={'news'} className={classes.menuItemSecondMenu}>
+            Высший мужской разряд
+          </Link>
+          <Link href={'news'} className={classes.menuItemSecondMenu}>
+            Мужской разряд
+          </Link>
+          <Link href={'news'} className={classes.menuItemSecondMenu}>
+            Женский разряд
+          </Link>
+        </div>
+        <Link href={'news'} className={classes.menuItem}>
+          VIP-Кабинеты
+        </Link>
+        <Link href={'news'} className={classes.menuItem}>
+          Услуги
+        </Link>
+        <Link href={'news'} className={classes.menuItem}>
+          Новости и акции
+        </Link>
+        <Link href={'news'} className={classes.menuItem}>
+          Кухня и бар
+        </Link>
+        <Link href={'services'} className={classes.menuItem}>
+          Контакты
+        </Link>
+      </div>
+    </>
   );
 };
