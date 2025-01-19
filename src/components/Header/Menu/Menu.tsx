341bb72387466af9f52dae989c3a585334637085
aemorozov <admin@aemorozov.com>
2025-01-19 10:38:56 +0200

    menu is ready, none active

--- src/components/Header/Menu/Menu.tsx
+++ src/components/Header/Menu/Menu.tsx
@@ -3,14 +3,62 @@ import classes from './Menu.module.css';
 import arrowDown from '../../../img/header/IconDown.svg';
 import Image from 'next/image';
 import secondMenuItemImg from '../../../img/header/Icon_container.svg';
+import { useState } from 'react';
 
 export const Menu = () => {
+  const [openSubMenu, isOpenSubMenu] = useState('');
+  const [addWidthMainItem, isAddWidthMainItem] = useState('');
+
+  const handleMouseEnter = (e: string) => {
+    isOpenSubMenu(e);
+    isAddWidthMainItem(classes.hoverFirstItem);
+  };
+
+  const handleMouseLeave = (e: string) => {
+    isOpenSubMenu(e);
+    isAddWidthMainItem('');
+  };
+
   return (
     <>
       <div className={classes.mainBlock}>
+        <div
+          className={classes.containerSecondMenu + ' ' + openSubMenu}
+          onMouseEnter={() => handleMouseEnter(classes.containerSecondMenuOpen)}
+          onMouseLeave={() => handleMouseLeave('')}
+        >
+          <div className={classes.mainBlockSecondMenu}>
+            <Link href={'services'} className={classes.menuItemSecondMenu}>
+              <Image
+                src={secondMenuItemImg}
+                alt="secondMenuItemImg"
+                className={classes.secondMenuItemImg}
+              ></Image>
+              Высший мужской разряд
+            </Link>
+            <Link href={'news'} className={classes.menuItemSecondMenu}>
+              <Image
+                src={secondMenuItemImg}
+                alt="secondMenuItemImg"
+                className={classes.secondMenuItemImg}
+              ></Image>
+              Мужской разряд
+            </Link>
+            <Link href={'news'} className={classes.menuItemSecondMenu}>
+              <Image
+                src={secondMenuItemImg}
+                alt="secondMenuItemImg"
+                className={classes.secondMenuItemImg}
+              ></Image>
+              Женский разряд
+            </Link>
+          </div>
+        </div>
         <Link
           href={'news'}
-          className={classes.menuItem + ' ' + classes.hoverItem}
+          className={classes.menuItem + ' ' + addWidthMainItem}
+          onMouseEnter={() => handleMouseEnter(classes.containerSecondMenuOpen)}
+          onMouseLeave={() => handleMouseLeave('')}
         >
           Разряды
           <Image
@@ -20,34 +68,6 @@ export const Menu = () => {
             width={16}
             height={15}
           />
-          <div className={classes.containerSecondMenu}>
-            <div className={classes.mainBlockSecondMenu}>
-              <Link href={'services'} className={classes.menuItemSecondMenu}>
-                <Image
-                  src={secondMenuItemImg}
-                  alt="secondMenuItemImg"
-                  className={classes.secondMenuItemImg}
-                ></Image>
-                Высший мужской разряд
-              </Link>
-              <Link href={'news'} className={classes.menuItemSecondMenu}>
-                <Image
-                  src={secondMenuItemImg}
-                  alt="secondMenuItemImg"
-                  className={classes.secondMenuItemImg}
-                ></Image>
-                Мужской разряд
-              </Link>
-              <Link href={'news'} className={classes.menuItemSecondMenu}>
-                <Image
-                  src={secondMenuItemImg}
-                  alt="secondMenuItemImg"
-                  className={classes.secondMenuItemImg}
-                ></Image>
-                Женский разряд
-              </Link>
-            </div>
-          </div>
         </Link>
         <Link href={'news'} className={classes.menuItem}>
           VIP-Кабинеты
