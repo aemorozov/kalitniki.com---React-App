91f9667f18e9bcbcdff15de46204e6fbd2ab3111
aemorozov <mr.aemorozov@gmail.com>
2025-03-23 14:47:36 +0200

    start index page vip block

--- src/components/ClassicBlock/ClassicBlock.tsx
+++ src/components/ClassicBlock/ClassicBlock.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
+import Image from 'next/image';
 import classes from './ClassicBlock.module.css';
 import { ThreeColumns } from './ThreeColumns/ThreeColumns';
+import vipBackImg from '../../img/mainPage/vip/d4d5a7239e7aac5883c5d8bea7488137.png';
+
+const blocksData = [
+  {
+    headerH2: 'Калитниковские бани',
+    headerH3: 'Общественные разряды',
+    content: <ThreeColumns />,
+    backgroundImage: '',
+    backgroundImageAlt: 'nothing'
+  },
+  {
+    headerH2: 'VIP-кабинеты',
+    headerH3: 'Для отдыха <br /> в компании близких',
+    content: '',
+    backgroundImage: vipBackImg,
+    backgroundImageAlt: 'Woman'
+  }
+];
 
 export function ClassicBlock() {
-  return (
-    <div className={classes.classicBlock}>
-      <div className={classes.contentBlock}>
-        <div className={classes.headers}>
-          <h2 className={classes.headerH2}>Калитниковские бани</h2>
-          <h3 className={classes.headerH3}>Общественные разряды</h3>
-        </div>
-        <ThreeColumns />
-        <div className={classes.bottomDescription}>
-          <p>
-            Cвежий пар{' '}
-            <span className={classes.bottomDescriptionSpan}>на травах</span>{' '}
-            каждые{' '}
-            <span className={classes.bottomDescriptionSpan}>40 минут</span> для
-            всех гостей наших бань!
-          </p>
+  return blocksData.map((block, i) => {
+    return (
+      <div className={classes.classicBlock} key={i}>
+        <Image
+          src={block.backgroundImage}
+          alt={block.backgroundImageAlt}
+          className={classes.backgroundImageVIP}
+        ></Image>
+        <div className={classes.contentBlock}>
+          <div className={classes.headers}>
+            <h2 className={classes.headerH2}>{block.headerH2}</h2>
+            <h3 className={classes.headerH3}>
+              {block.headerH3.split('<br />').map((line, i) => (
+                <React.Fragment key={i}>
+                  {i > 0 && <br />}
+                  {line}
+                </React.Fragment>
+              ))}
+            </h3>
+          </div>
+          {block.content}
         </div>
       </div>
-    </div>
-  );
+    );
+  });
 }
--- src/components/ClassicBlock/ThreeColumns/ThreeColumns.tsx
+++ src/components/ClassicBlock/ThreeColumns/ThreeColumns.tsx
@@ -7,137 +7,147 @@ import img3 from '../../../img/mainPage/razryadi/088edf0157c22276f535233deb19efd
 
 export function ThreeColumns() {
   return (
-    <div className={classes.mainBlock}>
-      <div className={classes.block}>
-        <div className={classes.divForImg}>
-          <Image src={img1} className={classes.img} alt="" title="" />
-        </div>
-        <div className={classes.descriptionBlock}>
-          <div>
-            <h2 className={classes.header}>Мужской разряд</h2>
-            <div className={classes.tags}>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Русская парная</p>
+    <>
+      <div className={classes.mainBlock}>
+        <div className={classes.block}>
+          <div className={classes.divForImg}>
+            <Image src={img1} className={classes.img} alt="" title="" />
+          </div>
+          <div className={classes.descriptionBlock}>
+            <div>
+              <h2 className={classes.header}>Мужской разряд</h2>
+              <div className={classes.tags}>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Русская парная</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Бассейн</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Хамам</p>
+                </div>
               </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Бассейн</p>
-              </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Хамам</p>
+              <div className={classes.scheduleBlock}>
+                <p className={classes.scheduleDay}>Понедельник</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Вторник</p>
+                <p className={classes.scheduleTime}>13:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Среда</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Четверг</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Пятница</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Суббота</p>
+                <p className={classes.scheduleTime}>09:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Воскресенье</p>
+                <p className={classes.scheduleTime}>09:00 — 23:00</p>
               </div>
             </div>
-            <div className={classes.scheduleBlock}>
-              <p className={classes.scheduleDay}>Понедельник</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Вторник</p>
-              <p className={classes.scheduleTime}>13:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Среда</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Четверг</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Пятница</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Суббота</p>
-              <p className={classes.scheduleTime}>09:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Воскресенье</p>
-              <p className={classes.scheduleTime}>09:00 — 23:00</p>
+            <div>
+              <div className={classes.priceBlock}>
+                <p className={classes.price}>От 1905 &#8381;/2 часа</p>
+                <p className={classes.priceDescription}>
+                  После 2х часов действует поминутная тарификация
+                </p>
+              </div>
+              <div className={classes.moreButtonBlock}>
+                <button className={classes.moreButton}>Подробнее</button>
+              </div>
             </div>
           </div>
-          <div>
-            <div className={classes.priceBlock}>
-              <p className={classes.price}>От 1905 &#8381;/2 часа</p>
-              <p className={classes.priceDescription}>
-                После 2х часов действует поминутная тарификация
-              </p>
+        </div>
+        <div className={classes.block}>
+          <div className={classes.divForImg}>
+            <Image src={img2} className={classes.img} alt="" title="" />
+          </div>
+          <div className={classes.descriptionBlock}>
+            <div>
+              <h2 className={classes.header}>Высший мужской разряд</h2>
+              <div className={classes.tags}>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Русская парная</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Бассейн</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Купель</p>
+                </div>
+              </div>
+              <div className={classes.scheduleBlock}>
+                <p className={classes.scheduleDay}>Среда</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Четверг</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Пятница</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Суббота</p>
+                <p className={classes.scheduleTime}>09:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Воскресенье</p>
+                <p className={classes.scheduleTime}>09:00 — 23:00</p>
+              </div>
             </div>
-            <div className={classes.moreButtonBlock}>
-              <button className={classes.moreButton}>Подробнее</button>
+            <div>
+              <div className={classes.priceBlock}>
+                <p className={classes.price}>От 2500 &#8381;/2 часа</p>
+                <p className={classes.priceDescription}>
+                  После 2х часов действует поминутная тарификация
+                </p>
+              </div>
+              <div className={classes.moreButtonBlock}>
+                <button className={classes.moreButton}>Подробнее</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.block}>
+          <div className={classes.divForImg}>
+            <Image src={img3} className={classes.img} alt="" title="" />
+          </div>
+          <div className={classes.descriptionBlock}>
+            <div>
+              <h2 className={classes.header}>Женский разряд</h2>
+              <div className={classes.tags}>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Русская парная</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Бассейн</p>
+                </div>
+                <div className={classes.tag}>
+                  <p className={classes.tagP}>Хамам</p>
+                </div>
+              </div>
+              <div className={classes.scheduleBlock}>
+                <p className={classes.scheduleDay}>Понедельник</p>
+                <p className={classes.scheduleTime}>13:00 — 23:00</p>
+                <p className={classes.scheduleDay}>Вторник</p>
+                <p className={classes.scheduleTime}>11:00 — 23:00</p>
+              </div>
+            </div>
+            <div>
+              <div className={classes.priceBlock}>
+                <p className={classes.price}>От 1905 &#8381;/2 часа</p>
+                <p className={classes.priceDescription}>
+                  После 2х часов действует поминутная тарификация
+                </p>
+              </div>
+              <div className={classes.moreButtonBlock}>
+                <button className={classes.moreButton}>Подробнее</button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-      <div className={classes.block}>
-        <div className={classes.divForImg}>
-          <Image src={img2} className={classes.img} alt="" title="" />
-        </div>
-        <div className={classes.descriptionBlock}>
-          <div>
-            <h2 className={classes.header}>Высший мужской разряд</h2>
-            <div className={classes.tags}>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Русская парная</p>
-              </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Бассейн</p>
-              </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Купель</p>
-              </div>
-            </div>
-            <div className={classes.scheduleBlock}>
-              <p className={classes.scheduleDay}>Среда</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Четверг</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Пятница</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Суббота</p>
-              <p className={classes.scheduleTime}>09:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Воскресенье</p>
-              <p className={classes.scheduleTime}>09:00 — 23:00</p>
-            </div>
-          </div>
-          <div>
-            <div className={classes.priceBlock}>
-              <p className={classes.price}>От 2500 &#8381;/2 часа</p>
-              <p className={classes.priceDescription}>
-                После 2х часов действует поминутная тарификация
-              </p>
-            </div>
-            <div className={classes.moreButtonBlock}>
-              <button className={classes.moreButton}>Подробнее</button>
-            </div>
-          </div>
-        </div>
+      <div className={classes.bottomDescription}>
+        <p>
+          Cвежий пар{' '}
+          <span className={classes.bottomDescriptionSpan}>на травах</span>{' '}
+          каждые <span className={classes.bottomDescriptionSpan}>40 минут</span>{' '}
+          для всех гостей наших бань!
+        </p>
       </div>
-      <div className={classes.block}>
-        <div className={classes.divForImg}>
-          <Image src={img3} className={classes.img} alt="" title="" />
-        </div>
-        <div className={classes.descriptionBlock}>
-          <div>
-            <h2 className={classes.header}>Женский разряд</h2>
-            <div className={classes.tags}>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Русская парная</p>
-              </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Бассейн</p>
-              </div>
-              <div className={classes.tag}>
-                <p className={classes.tagP}>Хамам</p>
-              </div>
-            </div>
-            <div className={classes.scheduleBlock}>
-              <p className={classes.scheduleDay}>Понедельник</p>
-              <p className={classes.scheduleTime}>13:00 — 23:00</p>
-              <p className={classes.scheduleDay}>Вторник</p>
-              <p className={classes.scheduleTime}>11:00 — 23:00</p>
-            </div>
-          </div>
-          <div>
-            <div className={classes.priceBlock}>
-              <p className={classes.price}>От 1905 &#8381;/2 часа</p>
-              <p className={classes.priceDescription}>
-                После 2х часов действует поминутная тарификация
-              </p>
-            </div>
-            <div className={classes.moreButtonBlock}>
-              <button className={classes.moreButton}>Подробнее</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
