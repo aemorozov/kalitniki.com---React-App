8834bea430b65f676cca0920406c92ce1516ddd2
aemorozov <mr.aemorozov@gmail.com>
2025-04-19 11:57:14 +0300

    add Kyhnya html css only

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,9 @@ import { ClassicBlock } from './../components/ClassicBlock/ClassicBlock';
 import Image from 'next/image';
 import turinImg from '../img/mainPage/vip/543150a55361764f23d8a1494d0cb30e.jpeg';
 import listItem from '../img/mainPage/vip/Bullet.svg';
+import uslugiImg1 from '../img/mainPage/uslugi/img1.jpeg';
+import uslugiImg2 from '../img/mainPage/uslugi/img2.jpeg';
+import uslugiImg3 from '../img/mainPage/uslugi/img3.jpeg';
 
 export default () => {
   return (
@@ -19,20 +22,21 @@ export default () => {
           </div>
           <div className={'slides'}>
             <div className={'slidesBlock reverse'}>
-              <Image src={turinImg} alt="Turin" className={'image'}></Image>
+              <Image src={uslugiImg3} alt="Turin" className={'image2'}></Image>
               <div className={'textBlock'}>
-                <h3 className={'header'}>Турин</h3>
+                <h3 className={'header'}>Услуги для тела</h3>
                 <p className={'text'}>
-                  Находится в высшем мужском разряде. Лучшее место для встречи c
-                  друзьями и близкими между парениями. Небольшой и уютный уголок
-                  для отдыха и релакса...
+                  Мы разработали программы, позволяющие восстановить здоровье
+                  тела, не выбиваясь из плотного графика города. У нас работают
+                  профессиональные пармейстеры — банщики и мастера массажа
                 </p>
                 <div className={'labels'}>
-                  <div className={'label'}>Для встреч c друзьями</div>
-                  <div className={'label'}>Обслуживание по меню</div>
-                  <div className={'label'}>Спутниковое TV</div>
-                  <div className={'label'}>Label</div>
-                  <div className={'label'}>Label</div>
+                  <div className={'label'}>Индивидуально</div>
+                  <div className={'label'}>Профессионально</div>
+                  <div className={'label'}>С любовью</div>
+                  <div className={'label'}>Мягкий пар</div>
+                  <div className={'label'}>Ароматы трав</div>
+                  <div className={'label'}>Забота рук</div>
                 </div>
                 <ul className={'plusses'}>
                   <li className={'plus'}>
@@ -41,7 +45,7 @@ export default () => {
                       className={'list'}
                       alt="list element"
                     ></Image>
-                    Вместимость до 6 гостей
+                    Фирменное контрастное парение
                   </li>
                   <li className={'plus'}>
                     <Image
@@ -49,7 +53,7 @@ export default () => {
                       className={'list'}
                       alt="list element"
                     ></Image>
-                    Общая площадь 12 м2
+                    Оздоровительные процедуры
                   </li>
                   <li className={'plus'}>
                     <Image
@@ -57,15 +61,100 @@ export default () => {
                       className={'list'}
                       alt="list element"
                     ></Image>
-                    Минимальная аренда — 2 часа
+                    Уход по телу
                   </li>
                 </ul>
-                <p className={'price'}>2500 P/2 часа</p>
+                <p className={'price'}>От 1500 P/2 часа</p>
                 <p className={'description'}>
-                  Оплачивается дополнительно к входному билету
-                  <br />
-                  После 2 часов действует поминутная тарификация
+                  Прогреваем и наполняем жизненной энергией, снимаем напряжение
                 </p>
+                <button className={'button'}>Все услуги</button>
+              </div>
+            </div>
+          </div>
+          <div className={'bottomDescription'}>
+            <p>
+              У нас работают профессиональные пармейстеры — банщики и мастера
+              массажа, которые сделают отдых не только приятным, но и
+              оздоравливающим!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className={'classicBlock  kyhnya - Posle aromatnoi parilki'}>
+        <div className={'contentBlock'}>
+          <div className={'headers'}>
+            <h2 className={'headerH2'}>Кухня</h2>
+            <h3 className={'headerH3'}>
+              После <br />
+              ароматной парилки
+            </h3>
+          </div>
+          <div className={'threeColumns'}>
+            <div className={'column'}>
+              <Image
+                src={uslugiImg1}
+                alt="Uslugi1"
+                className={'uslugiImg'}
+              ></Image>
+            </div>
+            <div className={'column'}>
+              <Image
+                src={uslugiImg2}
+                alt="Uslugi2"
+                className={'uslugiImg'}
+              ></Image>
+            </div>
+            <div className={'column'}>
+              <div className={'textBlock littleTextBlock'}>
+                <h3 className={'header'}>Кухня и бар </h3>
+                <p className={'text'}>
+                  Большой выбор холодных закусок, напитков, мясных и рыбных
+                  блюд, домашних настоек
+                </p>
+
+                <ul className={'plusses'}>
+                  <li className={'plus'}>
+                    <Image
+                      src={listItem}
+                      className={'list'}
+                      alt="list element"
+                    ></Image>
+                    Блюда на мангале
+                  </li>
+                  <li className={'plus'}>
+                    <Image
+                      src={listItem}
+                      className={'list'}
+                      alt="list element"
+                    ></Image>
+                    Домашние настойки
+                  </li>
+                  <li className={'plus'}>
+                    <Image
+                      src={listItem}
+                      className={'list'}
+                      alt="list element"
+                    ></Image>
+                    Лепим сами
+                  </li>
+                  <li className={'plus'}>
+                    <Image
+                      src={listItem}
+                      className={'list'}
+                      alt="list element"
+                    ></Image>
+                    Свежее разливное пиво
+                  </li>
+                  <li className={'plus'}>
+                    <Image
+                      src={listItem}
+                      className={'list'}
+                      alt="list element"
+                    ></Image>
+                    Сезонное меню
+                  </li>
+                </ul>
                 <button className={'button'}>Подробнее</button>
               </div>
             </div>
