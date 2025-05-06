0912ce82e50a81348d8dceeb0e3ed50262c36eb5
aemorozov <mr.aemorozov@gmail.com>
2025-05-06 08:42:17 +0300

    add robot.txt

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from 'next/app';
 import { Layout } from '../components/Layout';
+import Head from 'next/head';
 import '../styles/style.css';
 import '../styles/colors.css';
 
 export default ({ Component, pageProps }: AppProps) => {
   return (
     <Layout>
+      <Head>
+        <meta name="robots" content="noindex, nofollow" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
