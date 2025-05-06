3f22d2ff7a8aa84864c203bb31678e6b3575af87
aemorozov <mr.aemorozov@gmail.com>
2025-05-06 21:17:47 +0300

    fixed strange changes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,9 @@
 import Image from 'next/image';
 import Link from 'next/link';
-
+import { Menu } from './Menu';
+import { ContactsZone } from './ContactsZone';
+import classes from './Header.module.css';
+import logo from '/public/img/header/Logo.svg';
 
 export const Header = () => {
   return (
@@ -11,7 +14,6 @@ export const Header = () => {
           alt="Калитниковские бани лого"
           // className={classes.mainLogo}
           priority
-
         />
       </Link>
       <Menu />
