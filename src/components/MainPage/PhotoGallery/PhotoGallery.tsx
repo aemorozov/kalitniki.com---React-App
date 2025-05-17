2ddac3dad04ece686b656a6c529e8b624cede8ff
aemorozov <mr.aemorozov@gmail.com>
2025-05-17 15:32:11 +0300

    fixed 1920px some blocks

--- src/components/MainPage/PhotoGallery/PhotoGallery.tsx
+++ src/components/MainPage/PhotoGallery/PhotoGallery.tsx
@@ -8,55 +8,53 @@ import galaryImg4 from '/public/img/mainPage/fotogalereya/img4.jpg';
 
 export const PhotoGallery = () => {
   return (
-    <section className={styles.backgroundDoVsctrechi}>
-      <div className={'classicBlock' + ' ' + styles.doVsctrechi}>
-        <div className={'contentBlock'}>
-          <div className={'headers'}>
-            <h2 className={'headerH2'}>Фотогалерея</h2>
-            <h3 className={'headerH3'}>
-              До встречи
-              <br />в Калитниках
-            </h3>
+    <section className={'classicBlock' + ' ' + styles.doVsctrechi}>
+      <div className={'contentBlock'}>
+        <div className={'headers'}>
+          <h2 className={'headerH2'}>Фотогалерея</h2>
+          <h3 className={'headerH3'}>
+            До встречи
+            <br />в Калитниках
+          </h3>
+        </div>
+        <div className={'do-vsctrechi-slider'}>
+          <div className={styles.buttons}>
+            <div className={styles.leftButton}>
+              <Image
+                src={chevron}
+                alt="chevron"
+                className={styles.toLeft}
+              ></Image>
+            </div>
+            <div className={styles.rightButton}>
+              <Image
+                src={chevron}
+                alt="chevron"
+                className={styles.toRight}
+              ></Image>
+            </div>
           </div>
-          <div className={styles.doVsctrechiSlider}>
-            <div className={styles.buttons}>
-              <div className={styles.leftButton}>
-                <Image
-                  src={chevron}
-                  alt="chevron"
-                  className={styles.toLeft}
-                ></Image>
-              </div>
-              <div className={styles.rightButton}>
-                <Image
-                  src={chevron}
-                  alt="chevron"
-                  className={styles.toRight}
-                ></Image>
-              </div>
+          <div className={styles.doVsctrechiBlocks}>
+            <div className={styles.otzuv + ' ' + styles.galary}>
+              <Image src={galaryImg1} alt="img1"></Image>
             </div>
-            <div className={styles.doVsctrechiBlocks}>
-              <div className={styles.otzuv + ' ' + styles.galary}>
-                <Image src={galaryImg1} alt="img1"></Image>
-              </div>
-              <div className={styles.otzuv + ' ' + styles.galary}>
-                <Image src={galaryImg2} alt="img2"></Image>
-              </div>
-              <div className={styles.otzuv + ' ' + styles.galary}>
-                <Image src={galaryImg3} alt="img3"></Image>
-              </div>
-              <div className={styles.otzuv + ' ' + styles.galary}>
-                <Image src={galaryImg4} alt="img4"></Image>
-              </div>
+            <div className={styles.otzuv + ' ' + styles.galary}>
+              <Image src={galaryImg2} alt="img2"></Image>
             </div>
-            <div className={'bottomDescription' + ' correction'}>
-              <p>
-                Просторные{' '}
-                <span className={'bottomDescriptionSpan'}>парилки</span>, хамам,
-                купель — <br /> наслаждайтесь пространством и высоким качеством
-                пара!
-              </p>
+            <div className={styles.otzuv + ' ' + styles.galary}>
+              <Image src={galaryImg3} alt="img3"></Image>
             </div>
+            <div className={styles.otzuv + ' ' + styles.galary}>
+              <Image src={galaryImg4} alt="img4"></Image>
+            </div>
+          </div>
+          <div className={'bottomDescription' + ' correction'}>
+            <p>
+              Просторные{' '}
+              <span className={'bottomDescriptionSpan'}>парилки</span>, хамам,
+              купель — <br /> наслаждайтесь пространством и высоким качеством
+              пара!
+            </p>
           </div>
         </div>
       </div>
